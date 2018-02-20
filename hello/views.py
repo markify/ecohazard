dd25e7ac926fa4ba05f5b99689470fb13ef7b930
hCorbear <humeston@mail.sfsu.edu>
2018-02-19 19:21:13 -0800

    New application

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,16 @@
-import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import requests
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
-
+    #return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    #return HttpResponse('<pre>' + r.text + '</pre>')	
 
 def db(request):
 
