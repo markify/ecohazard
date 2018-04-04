df0aa609f5c4537e2c52e8da0a6df64e0783ad8c
Mark <merksmanship@gmail.com>
2018-04-03 22:57:03 -0700

    update urls to paths,
    add captcha, rest frame , and import hazard folder at settings.py
    add my own database config.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,26 +1,36 @@
-from django.conf.urls import include, url
-from django.urls import path
+"""Team 7 - URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+# Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('hazard/', include('hazard.urls'))
+"""
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import path, include
+import hazard.views as hazard_views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
+# path is an alternative way to do url in django 2.0
+# path ( website /  ,)   
+# import hazard.views as hazard_views    gets hazard -> views.py  make it called hazard_views
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^marker', hello.views.marker, name='marker'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^about(-\w*)?/?$', hello.views.about, name='about'),
-    url(r'^map(?:\.html?)?/?$', hello.views.map, name='map'),
-    url(r'^signup', hello.views.signup, name='signup'),
-    url(r'^login', hello.views.login, name='login'),
-    url(r'^report', hello.views.report, name='report'),
-    url(r'^search', hello.views.search, name='search'),
     path('admin/', admin.site.urls),
+    path('', include('hazard.urls')),
+    path('captcha/', include('captcha.urls')),
+    path('hazardposts/', hazard_views.AllHazardReportAPI.as_view(),
+         name="allhazardreportapi"),
+    path('hazardreport/<int:hazardreport_id>', hazard_views.hazardreportAPI,
+         name="hazardreportapi"),
+    path('userpostlist/<int:user_id>', hazard_views.userHazardPostListAPI, name="userpostlist"),
 ]
 
+urlpatterns = format_suffix_patterns(urlpatterns)
