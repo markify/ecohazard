f77c28ecf020b36c25e5279882a231e1335538f9
hCorbear <humeston@mail.sfsu.edu>
2018-04-11 10:13:08 -0700

    Added thumbnail support. Added thumbnails to front page.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'hazard'
+    'hazard',
+    'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
